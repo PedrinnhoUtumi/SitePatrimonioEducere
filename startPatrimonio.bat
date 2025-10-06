fbfd57f9a6dfc9911c186dd15bd327ee82103666
“Bellin1” <130059527+PedrinnhoUtumi@users.noreply.github.com>
2025-10-06 15:29:48 -0300

    Update startPatrimonio.bat

--- startPatrimonio.bat
+++ startPatrimonio.bat
@@ -2,6 +2,7 @@
 echo Iniciando o projeto...
 
 :: Abre o backend em uma nova janela
+cd /d "C:\Users\pcmec\Documents\GitHub\SitePatrimonioEducere"
 start cmd /k "npm run dev"
 
 echo Servidores iniciados.
