be598029f48d066ff7a450bbbe695983632fe80c
“Bellin1” <130059527+PedrinnhoUtumi@users.noreply.github.com>
2025-11-25 15:25:08 -0300

    Update _Layout.jsx

--- front/src/pages/_Layout.jsx
+++ front/src/pages/_Layout.jsx
@@ -5,7 +5,7 @@ import { ScrollToTop } from "../componentes/Layout/ScrollToTop";
 
 export function Layout() {
   return (
-    <div className="flex flex-col min-h-screen w-full overflow-x-hidden bg-white">
+    <div className="flex flex-col min-h-screen w-full overflow-x-hidden bg-black">
       <ScrollToTop behavior="smooth" />
       <MenuTop />
       <main className="flex flex-col flex-1 w-full max-w-full">
